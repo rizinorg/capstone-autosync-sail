771ef8498d65af9ed355aa08b906bff6a0dfb545
wargio <deroad@kumo.xn--q9jyb4c>
2025-03-27 17:31:51 +0800

    Generated enums outside of structs

--- lib/c_codegen/ccodegen_clike_typedef.ml
+++ lib/c_codegen/ccodegen_clike_typedef.ml
@@ -23,22 +23,38 @@ let stringify_clike_builin name builtin =
 let rec stringify_clike_typedef clike_typdef =
   match clike_typdef with
   | Clike_enum (typname, name, constants) ->
-      "enum " ^ typname ^ " {" ^ String.concat "," constants ^ "} " ^ name ^ ";"
+      let enum_ctype = if typname = "" then "uint64_t" else typname in
+      enum_ctype ^ " " ^ name ^ ";\n"
   | Clike_struct (typname, name, members) ->
       let members_as_str = List.map stringify_clike_typedef members in
-      "struct " ^ typname ^ " {"
+      "struct " ^ typname ^ " {\n"
       ^ String.concat "" members_as_str
-      ^ "} " ^ name ^ ";"
+      ^ "\n} " ^ name ^ ";"
   | Clike_union (typname, name, members) ->
       let members_as_str = List.map stringify_clike_typedef members in
-      "union " ^ typname ^ " {"
+      "union " ^ typname ^ " {\n"
       ^ String.concat "" members_as_str
-      ^ "} " ^ name ^ ";"
+      ^ "\n} " ^ name ^ ";"
   | Clike_builtin (name, bitvec) -> stringify_clike_builin name bitvec
   | Clike_void -> ""
   | Clike_typename (typname, name) -> typname ^ " " ^ name ^ ";"
 
+let rec stringify_enum_only_typedef clike_typdef =
+  match clike_typdef with
+  | Clike_enum (typname, name, constants) ->
+      "enum " ^ typname ^ " {\n" ^ String.concat ",\n" constants ^ "\n};\n"
+  | Clike_struct (typname, name, members) ->
+      let members_as_str = List.map stringify_enum_only_typedef members in
+      String.concat "" members_as_str
+  | Clike_union (typname, name, members) ->
+      let members_as_str = List.map stringify_enum_only_typedef members in
+      String.concat "" members_as_str
+  | Clike_builtin (name, bitvec) -> ""
+  | Clike_void -> ""
+  | Clike_typename (typname, name) -> ""
+
 let stringify_typdef typdef =
   "enum {" ^ identifier_prefix ^ "false = 0, " ^ identifier_prefix
-  ^ "true = 1}; "
+  ^ "true = 1};\n"
+  ^ stringify_enum_only_typedef typdef
   ^ stringify_clike_typedef typdef
